38f0326cdd59ee467b5628d8ab719202da876676
Christophe Cadilhac <christophe@alkeya.com>
2017-10-07 01:17:03 +0200

    Suppression de la continuation de trajet qui ne sert pas.

--- Barricades.Domaine/Trajet.cs
+++ Barricades.Domaine/Trajet.cs
@@ -20,26 +20,5 @@ namespace Barricades.Domaine
     {
       Etapes.Add(position);
     }
-
-    public Trajet ContinuerAvec(Trajet suite)
-    {
-      var trajetComplet = new Trajet();
-      foreach (var etape in Etapes)
-      {
-        trajetComplet.NouvelleEtape(etape);
-      }
-      foreach (var etape in suite.Etapes)
-      {
-        trajetComplet.NouvelleEtape(etape);
-      }
-      return trajetComplet;
-    }
-
-    public IEnumerable<Trajet> ContinuerAvec(params Trajet[] suites) => ContinuerAvec(suites.ToList());
-
-    public IEnumerable<Trajet> ContinuerAvec(IEnumerable<Trajet> suites)
-    {
-      return suites.Select(ContinuerAvec);
-    }
   }
 }
