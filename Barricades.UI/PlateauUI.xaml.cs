3ef78a2ba8418d6bcd09fb8ae96b3f41512e0349
Christophe Cadilhac <christophe@alkeya.com>
2017-10-07 23:10:06 +0200

    Projet UI.
    La UI basée sur plein de <Button/> ne me semble pas une bonne idée. Background de taille fixe qui représente les trous et les chemins. Rectangle avec background Pion positionnés statiquement.

---
+++ Barricades.UI/PlateauUI.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Barricades.Domaine;
+using static Barricades.Domaine.Position;
+
+namespace Barricades.UI
+{
+  public partial class PlateauUI : UserControl
+  {
+    public Plateau Plateau { get; private set; }
+
+    public PlateauUI()
+    {
+      InitializeComponent();
+
+      Plateau = new Plateau();
+      DessinerLePlateau();
+    }
+
+    private void DessinerLePlateau()
+    {
+      for (var y = 0; y < 9; y++)
+        DessinerTrou(Plateau[P($"0,{y}")], FindName($"_0{y}") as Button);
+
+      for (var y = 0; y < 9; y++)
+        DessinerTrou(Plateau[P($"1,{y}")], FindName($"_1{y}") as Button);
+
+      DessinerTrou(Plateau[P("2,1")], _21);
+    }
+
+    private void DessinerTrou(Trou trou, ContentControl ui)
+    {
+      ui.Background = (ImageBrush) Resources["ImageTrou"];
+      if (!trou.EstVide)
+      {
+        ui.Content = "<__>";
+        ui.Foreground = Couleurs[trou.Pion.Couleur];
+      }
+    }
+
+    private static readonly Dictionary<Couleur, SolidColorBrush> Couleurs = new Dictionary<Couleur, SolidColorBrush>
+    {
+      {Couleur.Bleu, new SolidColorBrush(Colors.CornflowerBlue)},
+      {Couleur.Jaune, new SolidColorBrush(Colors.Gold)},
+      {Couleur.Vert, new SolidColorBrush(Colors.OliveDrab)},
+      {Couleur.Rouge, new SolidColorBrush(Colors.Crimson)},
+      {Couleur.Barricade, new SolidColorBrush(Colors.SaddleBrown)},
+    };
+  }
+}
